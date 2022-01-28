dda0d3b0fb70e3bbf7342360ef11a9e802316ca6
SalmaRagab <salmaahragab@gmail.com>
2022-01-28 22:14:43 +0200

    add login action

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,13 +1,44 @@
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { constructAPIURL } from "../../apiUrl";
+
 export function Login() {
+  let navigate = useNavigate();
+
+  const [userName, setUserName] = useState("");
+
+  const login = (e) => {
+    e.preventDefault();
+    fetch(constructAPIURL("/login"), {
+      method: "POST",
+      body: JSON.stringify({ userName }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
+            userName: userName,
+            token: result.token,
+          })
+        );
+        navigate("../countries", { replace: true });
+      });
+  };
+
   return (
-    <Form>
+    <Form onSubmit={login}>
       <Form.Group className="mb-3" controlId="userName">
         <Form.Label>User Name</Form.Label>
-        <Form.Control placeholder="Enter User Name" />
+        <Form.Control
+          value={userName}
+          placeholder="Enter User Name"
+          onChange={(e) => setUserName(e.target.value)}
+        />
       </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
+      <Button variant="primary" type="login">
+        Login
       </Button>
     </Form>
   );
