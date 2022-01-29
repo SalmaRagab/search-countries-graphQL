4dee115795585b5bf242998acc5d453ac3cfd960
SalmaRagab <salmaahragab@gmail.com>
2022-01-29 23:20:27 +0200

    add usercontroller and remove the user service

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-module.exports = class UserService {
+module.exports = class UserController {
   login(userName) {
     const token = jwt.sign({ userName: userName }, process.env.TOKEN_KEY, {
       expiresIn: process.env.TOKEN_EXPIRATION,
--- backend/resolvers/resolvers.js
+++ backend/resolvers/resolvers.js
@@ -1,11 +1,11 @@
+const UserController = require("../controllers/UserController");
 const CountriesService = require("../services/CountriesService");
-const UserService = require("../services/UserService");
 
 const countriesService = new CountriesService();
-const userService = new UserService();
+const userController = new UserController();
 
 const Query = {
-  login: (root, args) => userService.login(args.userName),
+  login: (root, args) => userController.login(args.userName),
   countries: (root, args) => countriesService.getCountries(args.countryName),
 };
 module.exports = { Query };
